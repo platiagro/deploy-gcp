c8d249b4db1de83fda9ef668415e9842eb2ba989
Fabio Beranizo Fontes Lopes <fabiol@cpqd.com.br>
2021-05-20 17:58:48 -0300

    Updates get_deployment_status to check for https URL
    Latest version uses https by default.

--- backend/platiagro/deployment.py
+++ backend/platiagro/deployment.py
@@ -46,7 +46,7 @@ def get_deployment_status(params: dict) -> dict:
         ip = proc.stdout
         m = match(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", ip)
         if ip is not None and m is not None:
-            url = f"http://{ip}"
+            url = f"https://{ip}"
             # verify the platform is up and running
             resp = get(url, verify=False, timeout=TIMEOUT_IN_SECONDS)
             resp.raise_for_status()
